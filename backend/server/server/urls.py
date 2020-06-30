6653174ccde128b3143adf884804a19f44944914
Yosef Sheinfil <ysheinfil@centershealthcare.org>
2020-06-30 19:06:17 -0400

    endpoints models

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.endpoints.urls import urlpatterns as endpoint_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += endpoint_urlpatterns
